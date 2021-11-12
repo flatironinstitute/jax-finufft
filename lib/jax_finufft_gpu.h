1b12da0bb44f02a516f59d666de24c4b0fd302c3
Lehman Garrison <lgarrison@flatironinstitute.org>
2021-11-12 11:38:04 -0500

    Maybe sort-of calling all the right functions?

--- lib/jax_finufft_gpu.h
+++ lib/jax_finufft_gpu.h
@@ -1,5 +1,5 @@
-#ifndef _JAX_FINUFFT_H_
-#define _JAX_FINUFFT_H_
+#ifndef _JAX_FINUFFT_GPU_H_
+#define _JAX_FINUFFT_GPU_H_
 
 #include <complex>
 
@@ -14,39 +14,39 @@ template <>
 struct plan_type<double> {
   typedef cufinufft_plan type;
 };
-/*
+
 template <>
 struct plan_type<float> {
-  typedef finufftf_plan type;
+  typedef cufinufftf_plan type;
 };
 
 template <typename T>
-void default_opts(nufft_opts* opts);
+void default_opts(int type, int dim, cufinufft_opts* opts);
 
 template <>
-void default_opts<float>(nufft_opts* opts) {
-  finufftf_default_opts(opts);
+void default_opts<float>(int type, int dim, cufinufft_opts* opts) {
+  cufinufftf_default_opts(type, dim, opts);
 }
 
 template <>
-void default_opts<double>(nufft_opts* opts) {
-  finufft_default_opts(opts);
+void default_opts<double>(int type, int dim, cufinufft_opts* opts) {
+  cufinufft_default_opts(type, dim, opts);
 }
 
 template <typename T>
-int makeplan(int type, int dim, int64_t* nmodes, int iflag, int ntr, T eps,
-             typename plan_type<T>::type* plan, nufft_opts* opts);
+int makeplan(int type, int dim, int* nmodes, int iflag, int ntr, T eps, int batch,
+             typename plan_type<T>::type* plan, cufinufft_opts* opts);
 
 template <>
-int makeplan<float>(int type, int dim, int64_t* nmodes, int iflag, int ntr, float eps,
-                    typename plan_type<float>::type* plan, nufft_opts* opts) {
-  return finufftf_makeplan(type, dim, nmodes, iflag, ntr, eps, plan, opts);
+int makeplan<float>(int type, int dim, int* nmodes, int iflag, int ntr, float eps, int batch,
+                    typename plan_type<float>::type* plan, cufinufft_opts* opts) {
+  return cufinufftf_makeplan(type, dim, nmodes, iflag, ntr, eps, batch, plan, opts);
 }
 
 template <>
-int makeplan<double>(int type, int dim, int64_t* nmodes, int iflag, int ntr, double eps,
-                     typename plan_type<double>::type* plan, nufft_opts* opts) {
-  return finufft_makeplan(type, dim, nmodes, iflag, ntr, eps, plan, opts);
+int makeplan<double>(int type, int dim, int* nmodes, int iflag, int ntr, double eps, int batch,
+                     typename plan_type<double>::type* plan, cufinufft_opts* opts) {
+  return cufinufft_makeplan(type, dim, nmodes, iflag, ntr, eps, batch, plan, opts);
 }
 
 template <typename T>
@@ -56,13 +56,13 @@ int setpts(typename plan_type<T>::type plan, int64_t M, T* x, T* y, T* z, int64_
 template <>
 int setpts<float>(typename plan_type<float>::type plan, int64_t M, float* x, float* y, float* z,
                   int64_t N, float* s, float* t, float* u) {
-  return finufftf_setpts(plan, M, x, y, z, N, s, t, u);
+  return cufinufftf_setpts(M, x, y, z, N, s, t, u, plan);
 }
 
 template <>
 int setpts<double>(typename plan_type<double>::type plan, int64_t M, double* x, double* y,
                    double* z, int64_t N, double* s, double* t, double* u) {
-  return finufft_setpts(plan, M, x, y, z, N, s, t, u);
+  return cufinufft_setpts(M, x, y, z, N, s, t, u, plan);
 }
 
 template <typename T>
@@ -71,13 +71,17 @@ int execute(typename plan_type<T>::type plan, std::complex<T>* c, std::complex<T
 template <>
 int execute<float>(typename plan_type<float>::type plan, std::complex<float>* c,
                    std::complex<float>* f) {
-  return finufftf_execute(plan, c, f);
+  cuFloatComplex* _c = reinterpret_cast<cuFloatComplex*>(c);
+  cuFloatComplex* _f = reinterpret_cast<cuFloatComplex*>(f);
+  return cufinufftf_execute(_c, _f, plan);
 }
 
 template <>
 int execute<double>(typename plan_type<double>::type plan, std::complex<double>* c,
                     std::complex<double>* f) {
-  return finufft_execute(plan, c, f);
+  cuDoubleComplex* _c = reinterpret_cast<cuDoubleComplex*>(c);
+  cuDoubleComplex* _f = reinterpret_cast<cuDoubleComplex*>(f);
+  return cufinufft_execute(_c, _f, plan);
 }
 
 template <typename T>
@@ -85,12 +89,12 @@ void destroy(typename plan_type<T>::type plan);
 
 template <>
 void destroy<float>(typename plan_type<float>::type plan) {
-  finufftf_destroy(plan);
+  cufinufftf_destroy(plan);
 }
 
 template <>
 void destroy<double>(typename plan_type<double>::type plan) {
-  finufft_destroy(plan);
+  cufinufft_destroy(plan);
 }
 
 template <int ndim, typename T>
@@ -122,7 +126,6 @@ template <>
 float* z_index<3, float>(float* z, int64_t index) {
   return &(z[index]);
 }
-*/
 
 }  // namespace jax_finufft
 
--- lib/kernels.cc.cu
+++ lib/kernels.cc.cu
@@ -9,6 +9,35 @@ void ThrowIfError(cudaError_t error) {
     throw std::runtime_error(cudaGetErrorString(error));
   }
 }
+    
+template <int ndim, typename T>
+void run_nufft(int type, const NufftDescriptor<T>* descriptor, T *x, T *y, T *z, std::complex<T> *c, std::complex<T> *F) {
+  int64_t n_k = 1;
+  for (int d = 0; d < ndim; ++d) n_k *= descriptor->n_k[d];
+    
+  // TODO: okay to stack-allocate this?
+  int nmodes32[ndim];
+  for (int d = 0; d < ndim; ++d) nmodes32[d] = static_cast<int>(descriptor->n_k[d]);
+
+  // TODO: does this need to be part of NufftDescriptor? It's GPU-specific.
+  int maxbatchsize = 0;  // auto
+  cufinufft_opts *opts = new cufinufft_opts;
+  typename plan_type<T>::type plan;
+  default_opts<T>(type, ndim, opts);
+  makeplan<T>(type, ndim, nmodes32, descriptor->iflag,
+              descriptor->n_transf, descriptor->eps, maxbatchsize, &plan, opts);
+  for (int64_t index = 0; index < descriptor->n_tot; ++index) {
+    int64_t j = index * descriptor->n_j * descriptor->n_transf;
+    int64_t k = index * n_k * descriptor->n_transf;
+
+    setpts<T>(plan, descriptor->n_j, &(x[j]), y_index<ndim, T>(y, j), z_index<ndim, T>(z, j), 0,
+              NULL, NULL, NULL);
+
+    execute<T>(plan, &c[j], &F[k]);
+  }
+  destroy<T>(plan);
+  delete opts;
+}
 
 
 template <int ndim, typename T>
@@ -31,7 +60,7 @@ void nufft1(cudaStream_t stream, void** buffers, const char* opaque, std::size_t
   std::complex<T> *F = reinterpret_cast<std::complex<T> *>(buffers[out_dim]);
   
   // Call cuFINUFFT here...
-  // run_nufft<ndim, T>(1, in[0], x, y, z, c, F);
+  run_nufft<ndim, T>(1, descriptor, x, y, z, c, F);
 
   ThrowIfError(cudaGetLastError());
 }
@@ -56,7 +85,7 @@ void nufft2(cudaStream_t stream, void** buffers, const char* opaque, std::size_t
   std::complex<T> *c = reinterpret_cast<std::complex<T> *>(buffers[out_dim]);
   
   // Call cuFINUFFT here...
-  // run_nufft<ndim, T>(1, in[0], x, y, z, c, F);
+  run_nufft<ndim, T>(1, descriptor, x, y, z, c, F);
 
   ThrowIfError(cudaGetLastError());
 }
