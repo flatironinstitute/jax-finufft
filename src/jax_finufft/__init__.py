8dd3345ae8b95fa9d1a82dec10b08b53fd89d4ef
Dan F-M <foreman.mackey@gmail.com>
2023-10-30 16:17:17 -0400

    Update copyright dates

--- src/jax_finufft/__init__.py
+++ src/jax_finufft/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2021 The Simons Foundation, Inc.
+# Copyright 2021, 2022, 2023 The Simons Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
