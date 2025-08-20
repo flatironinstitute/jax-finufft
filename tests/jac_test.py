6820cec8dffbfb70a00961d96db08024cd203eb8
unalmis <kayaunalmis@proton.me>
2025-08-20 05:33:26 -0500

    Add more tests

--- tests/jac_test.py
+++ tests/jac_test.py
@@ -1,3 +1,5 @@
+"""Test interpolation of real valued maps and their JVPs."""
+
 import jax.numpy as jnp
 from jax_finufft import nufft2, options
 import pytest
@@ -10,49 +12,91 @@ from jax import value_and_grad
 jax_config.update("jax_enable_x64", True)
 
 
-def nufft2r(
-    f,
-    x,
-    y,
-    domain_x=(0, 2 * jnp.pi),
-    domain_y=(0, 2 * jnp.pi),
-    rfft_axis=-1,
-    vec=False,
-    eps=1e-7,
-):
-    """Non-uniform real fast Fourier transform of second type.
+def nufft2r_1d(f, x, domain=(0, 2 * jnp.pi), vec=False, eps=1e-6):
+    """Non-uniform real fast Forier transform of second type.
 
     Notes
     -----
     Vectorization with the following signatures are supported.
-     - ``(f,c0,c1),(x),(x)->(f,x)``
+     - ``(b,f),(x)->(b,x)``
 
     Parameters
     ----------
-    c : jnp.ndarray
-        Fourier coefficients cₙ of the map x₀,x₁ ↦ c(x₀,x₁) such that
-        c(x₀,x₁) = ∑ₘₙ cₘₙ exp(i m x₀) exp(i n x₁).
+    f : jnp.ndarray
+        Fourier coefficients fₙ of the map x ↦ c(x) such that c(x) = ∑ₙ fₙ exp(i n x)
+        where n >= 0.
+    x : jnp.ndarray
+        Real query points of coordinate in ``domain`` where interpolation is desired.
+        The coordinates stored here must be the same coordinate enumerated across
+        axis ``-1`` of ``f``.
+    domain : tuple[float]
+        Domain of coordinate specified by x over which samples were taken.
+    vec : bool
+        If set to ``True``, then it is assumed that multiple Fourier series are
+        to be evaluated at the same non-uniform points. In that case, this flag
+        must be set to retain the function signature of ``(b,f),(x)->(b,x)``.
+    eps : float
+        Precision requested. Default is ``1e-6``.
+
+    Returns
+    -------
+    c(x) : jnp.ndarray
+        Real function value at query points.
+
+    """
+    s = 2 * jnp.pi / (domain[1] - domain[0])
+    x = (x - domain[0]) * s
+
+    s = f.shape[-1] // 2
+    s = jnp.exp(1j * s * x)
+    s = s[..., jnp.newaxis, :] if vec else s
+
+    opts = options.Opts(modeord=0)
+    return (nufft2(f, x, iflag=1, eps=eps, opts=opts) * s).real
+
+
+def nufft2r_2d(
+    f,
+    x0,
+    x1,
+    domain0=(0, 2 * jnp.pi),
+    domain1=(0, 2 * jnp.pi),
+    rfft_axis=-1,
+    vec=False,
+    eps=1e-6,
+):
+    """Non-uniform real fast Forier transform of second type.
+
+    Notes
+    -----
+    Vectorization with the following signatures are supported.
+     - ``(b,f0,f1),(x),(x)->(b,x)``
+
+    Parameters
+    ----------
+    f : jnp.ndarray
+        Fourier coefficients fₘₙ of the map x₀,x₁ ↦ c(x₀,x₁) such that
+        c(x₀,x₁) = ∑ₘₙ fₘₙ exp(i m x₀) exp(i n x₁).
     x0 : jnp.ndarray
         Real query points of coordinate in ``domain0`` where interpolation is desired.
-        The coordinates stored here must be the same coordinate enumerated across axis
-        ``-2`` of ``c``.
+        The coordinates stored here must be the same coordinate
+        enumerated across axis ``-2`` of ``f``.
     x1 : jnp.ndarray
         Real query points of coordinate in ``domain1`` where interpolation is desired.
-        The coordinates stored here must be the same coordinate enumerated across axis
-        ``-1`` of ``c``.
+        The coordinates stored here must be the same coordinate
+        enumerated across axis ``-1`` of ``f``.
     domain0 : tuple[float]
         Domain of coordinate specified by x₀ over which samples were taken.
     domain1 : tuple[float]
         Domain of coordinate specified by x₁ over which samples were taken.
     rfft_axis : int
         Axis along which real FFT was performed.
-        If -1 (-2), assumes c(x₀,x₁) = ∑ₘₙ cₘₙ exp(i m x₀) exp(i n x₁) where
+        If -1 (-2), assumes c(x₀,x₁) = ∑ₘₙ fₘₙ exp(i m x₀) exp(i n x₁) where
             n ( m) >= 0, respectively.
-        If ``None``, then assumes no real FFT was performed.
     vec : bool
         If set to ``True``, then it is assumed that multiple Fourier series are
         to be evaluated at the same non-uniform points. In that case, this flag
-        must be set to retain the function signature of ``(f,c0,c1),(x),(x)->(f,x)``.
+        must be set to retain the function signature of ``(b,f0,f1),(x),(x)->(b,x)``.
     eps : float
         Precision requested. Default is ``1e-6``.
 
@@ -62,26 +106,35 @@ def nufft2r(
         Real function value at query points.
 
     """
-    sx = 2 * jnp.pi / (domain_x[1] - domain_x[0])
-    sy = 2 * jnp.pi / (domain_y[1] - domain_y[0])
-    x = (x - domain_x[0]) * sx
-    y = (y - domain_y[0]) * sy
+    s0 = 2 * jnp.pi / (domain0[1] - domain0[0])
+    s1 = 2 * jnp.pi / (domain1[1] - domain1[0])
+    x0 = (x0 - domain0[0]) * s0
+    x1 = (x1 - domain1[0]) * s1
 
     if rfft_axis is None:
         s = 1
     elif rfft_axis != -1 and rfft_axis != -2:
-        raise NotImplementedError(f"rfft_axis must be -1 or -2, but got {rfft_axis}")
+        raise NotImplementedError(f"rfft_axis must be -1 or -2, but got {rfft_axis}.")
     else:
         s = f.shape[rfft_axis] // 2
-        s = jnp.exp(1j * s * (y if rfft_axis == -1 else x))
+        s = jnp.exp(1j * s * (x1 if rfft_axis == -1 else x0))
         s = s[..., jnp.newaxis, :] if vec else s
         f = jnp.fft.ifftshift(f, rfft_axis)
 
     opts = options.Opts(modeord=1)
-    return (nufft2(f, x, y, iflag=1, eps=eps, opts=opts) * s).real
+    return (nufft2(f, x0, x1, iflag=1, eps=eps, opts=opts) * s).real
 
 
-def _f_2d(x, y):
+def _c_1d(x):
+    """Test function for 1D FFT."""
+    return jnp.cos(7 * x) + jnp.sin(x) - 33.2
+
+
+def _c_1d_nyquist_freq():
+    return 7
+
+
+def _c_2d(x, y):
     """Test function for 2D FFT."""
     x_freq, y_freq = 3, 5
     return (
@@ -94,27 +147,62 @@ def _f_2d(x, y):
     )
 
 
-def _f_2d_nyquist_freq():
+def _c_2d_nyquist_freq():
     x_freq, y_freq = 3, 5
     x_freq_nyquist = x_freq + 2
     y_freq_nyquist = y_freq + 3
     return x_freq_nyquist, y_freq_nyquist
 
 
+_test_inputs_1D = [
+    (_c_1d, 2 * _c_1d_nyquist_freq() + 1, (0, 2 * jnp.pi)),
+    (_c_1d, 2 * _c_1d_nyquist_freq(), (0, 2 * jnp.pi)),
+    (_c_1d, 2 * _c_1d_nyquist_freq() + 1, (-jnp.pi, jnp.pi)),
+    (_c_1d, 2 * _c_1d_nyquist_freq(), (-jnp.pi, jnp.pi)),
+    (lambda x: jnp.cos(7 * x), 2, (-jnp.pi / 7, jnp.pi / 7)),
+    (lambda x: jnp.sin(7 * x), 3, (-jnp.pi / 7, jnp.pi / 7)),
+]
+
+
+@pytest.mark.parametrize("func, n, domain", _test_inputs_1D)
+def test_non_uniform_real_FFT(func, n, domain):
+    """Test non-uniform real FFT interpolation."""
+    x = jnp.linspace(domain[0], domain[1], n, endpoint=False)
+    c = func(x)
+    xq = jnp.array([7.34, 1.10134, 2.28])
+
+    f = 2 * jnp.fft.rfft(c, norm="forward")
+    f = f.at[..., (0, -1) if (n % 2 == 0) else 0].divide(2)
+    np.testing.assert_allclose(nufft2r_1d(f, xq, domain), func(xq))
+
+    @value_and_grad
+    def fun1(xq):
+        return nufft2r_1d(f, xq, domain, eps=1e-7).sum()
+
+    @value_and_grad
+    def truth(xq):
+        return func(xq).sum()
+
+    v, g = truth(xq)
+    f1v, f1g = fun1(xq)
+    np.testing.assert_allclose(f1v, v)
+    np.testing.assert_allclose(f1g, g)
+
+
 @pytest.mark.parametrize(
     "func, m, n, domain_x, domain_y",
     [
         (
-            _f_2d,
-            2 * _f_2d_nyquist_freq()[0] + 1,
-            2 * _f_2d_nyquist_freq()[1] + 1,
+            _c_2d,
+            2 * _c_2d_nyquist_freq()[0] + 1,
+            2 * _c_2d_nyquist_freq()[1] + 1,
             (0, 2 * jnp.pi),
             (0, 2 * jnp.pi),
         ),
         (
-            _f_2d,
-            2 * _f_2d_nyquist_freq()[0] + 1,
-            2 * _f_2d_nyquist_freq()[1] + 1,
+            _c_2d,
+            2 * _c_2d_nyquist_freq()[0] + 1,
+            2 * _c_2d_nyquist_freq()[1] + 1,
             (-jnp.pi / 3, 5 * jnp.pi / 3),
             (jnp.pi, 3 * jnp.pi),
         ),
@@ -142,16 +230,16 @@ def test_non_uniform_real_FFT_2D(func, m, n, domain_x, domain_y):
     f2 = jnp.fft.fft2(c, norm="forward")
 
     v = func(xq, yq)
-    np.testing.assert_allclose(nufft2r(f1, xq, yq, domain_x, domain_y), v)
-    np.testing.assert_allclose(nufft2r(f2, xq, yq, domain_x, domain_y, None), v)
+    np.testing.assert_allclose(nufft2r_2d(f1, xq, yq, domain_x, domain_y), v)
+    np.testing.assert_allclose(nufft2r_2d(f2, xq, yq, domain_x, domain_y, None), v)
 
     @value_and_grad
     def fun1(xq, yq):
-        return nufft2r(f1, xq, yq, domain_x, domain_y).sum()
+        return nufft2r_2d(f1, xq, yq, domain_x, domain_y, eps=1e-7).sum()
 
     @value_and_grad
     def fun2(xq, yq):
-        return nufft2r(f2, xq, yq, domain_x, domain_y, None).sum()
+        return nufft2r_2d(f2, xq, yq, domain_x, domain_y, None, eps=1e-7).sum()
 
     @value_and_grad
     def truth(xq, yq):
@@ -167,3 +255,37 @@ def test_non_uniform_real_FFT_2D(func, m, n, domain_x, domain_y):
     np.testing.assert_allclose(f1g, f2g, err_msg="Something wrong with modeord")
     np.testing.assert_allclose(f1g, g)
     np.testing.assert_allclose(f2g, g)
+
+
+def test_nufft2_vec():
+    """Test vectorized JAX-finufft interpolation.
+
+    https://github.com/flatironinstitute/jax-finufft/issues/155.
+    """
+    func_1, n, domain = _test_inputs_1D[0]
+    func_2 = lambda x: -77 * jnp.sin(7 * x) + 18 * jnp.cos(x) + 100  # noqa: E731
+    x = jnp.linspace(domain[0], domain[1], n, endpoint=False)
+    c = jnp.stack([func_2(x), func_2(x)])
+
+    xq = jnp.array([7.34, 1.10134, 2.28])
+    cq = jnp.stack([func_2(xq), func_2(xq)])
+
+    f = 2 * jnp.fft.rfft(c, norm="forward")
+    f = f.at[..., (0, -1) if (c.shape[-1] % 2 == 0) else 0].divide(2)
+
+    def _nufft2r(a, xq, vec=False):
+        return nufft2r_1d(a, xq, domain, vec)
+
+    # multiple (2) fourier series evaluated at the same (3) points.
+    # Much more efficient than naive vectorization for large # of points.
+    np.testing.assert_allclose(_nufft2r(f, xq), cq)
+
+    xq = jnp.stack([xq, xq**2, xq**3, xq**4])[jnp.newaxis]
+    f = jnp.stack([f, -f, 2 * f, 3 * f])[jnp.newaxis]
+
+    # vectorized over batch of size (1, 4), evaluating
+    # multiple (2) fourier series evaluated at the same (3) points
+    np.testing.assert_allclose(
+        _nufft2r(f, xq, vec=True),
+        jnp.vectorize(_nufft2r, signature="(b,f),(x)->(b,x)")(f, xq),
+    )
